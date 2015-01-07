796aec5658e2252d05ac15dff957df8e8e83e6df
Kojo Kumah <info@kojokumah.com>
2015-01-07 15:37:47 -0800

    Eclipse project for basic example

--- example-basic/src/main.cpp
+++ example-basic/src/main.cpp
@@ -1,10 +1,12 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-
-	ofSetupOpenGL(800,500, OF_WINDOW);			// <-------- setup the GL context
+	// Using GLUT because of Ubuntu 14.10 bug
+	ofAppGlutWindow window; // create a window
+	ofSetupOpenGL(&window, 800,500, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
